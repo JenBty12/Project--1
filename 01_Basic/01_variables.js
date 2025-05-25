bb7b041fed43a61fd36bf9fd39383fe4d3ea1ce2
JenBty12 <jenbty25.iitg@gmail.com>
2025-05-25 03:23:20 +0000

    pre

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,7 @@
+const accountID = 123;
+let accountEmail = "jenny@gmail.com";
+var accountPassword = "1234";
+var accountCity = "Delhi"; // Declare with let, const, or var
+var accountCity = "Bombay"; // Fix: use the same casing
+console.log(accountID);
+console.log([accountID, accountEmail, accountPassword, accountCity]);
---
+++ 01_Basic/02_DataFiles.js
@@ -0,0 +1,21 @@
+"use strict";//treat all JS code as newer version
+// alert (3+4) we Are using node js,not browser
+console.log(3+
+    3)//Code rediability should be high
+let name="jennny"
+let age=18
+let isLoggedIN=false
+let temp=null
+
+//number =>> 2 to power 53
+//bigint
+//string=>""
+//Boolean=>true/false
+//null=>standalone value
+//undefined=>Means value is not assign
+//symbol mostlu use in React
+
+//object 
+console.log(typeof"jenny");
+console.log(typeof"Undefined");//undefined
+console.log(typeof"null");//object
