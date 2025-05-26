ba9d06c371bdb80ed905c448066693769c985bf4
JenBty12 <jenbty25.iitg@gmail.com>
2025-05-26 16:45:51 +0000

    Comparison

---
+++ 01_Basic/04_Comparison.js
@@ -0,0 +1,9 @@
+//Data type is to be same in comaprison
+// comparison is a boolean value
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+//comparison and equality check is different in java script
+//=== is a strict check 
+//data type change means there will be a problem
+
