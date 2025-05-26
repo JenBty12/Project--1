e198c8a37b6ffea7ca47f3329569cd44fb7ff86f
JenBty12 <jenbty25.iitg@gmail.com>
2025-05-26 16:58:01 +0000

    data type

---
+++ 01_Basic/datatypessummary.js
@@ -0,0 +1,4 @@
+//data types on how data is being stored primitive and non primitive
+//7 types : its a string,number,boolean,null,undefined,symbol,BigInt-primitve datatype
+//Reference type or non primitive:arrays,objects and functions
+//java script is a 
